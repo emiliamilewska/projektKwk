b6fc796ecc9dcd9218d2adc302a9d2ebdb83a45d
emilka <emilia.milewska@infotech.edu.pl>
2026-05-20 12:38:24 +0200

    Add basic hero structure

---
+++ folder/rpg.py
@@ -0,0 +1,9 @@
+
+def pokaz_bohatera():
+    print("--- TWOJA POSTAĆ RPG ---")
+    print("Nazwa: - ")
+    print("Wiek: - ")
+    print("Klasa: Mag")
+
+if __name__ == "__main__":
+    pokaz_bohatera()
